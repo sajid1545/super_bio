07d43cc2c5b3b2b5d31a2ae8e017f2189ef471f2
sajid9962 <clashking1545@gmail.com>
2022-12-30 00:56:00 +0600

    tryForFree section created

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Banner from './Components/Banner/Banner';
 import Gamer from './Components/Gamer/Gamer';
 import Navbar from './Components/Navbar/Navbar';
 import SecondBanner from './Components/SecondBanner/SecondBanner';
+import TryForFree from './Components/TryForFree/TryForFree';
 import UseIt from './Components/UseIt/UseIt';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
 			<UseIt />
 			<About />
 			<Gamer />
+			<TryForFree />
 		</div>
 	);
 }
---
+++ src/Components/TryForFree/TryForFree.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import free from '../../assets/images/free.png';
+
+const TryForFree = () => {
+	return (
+		<div className="w-[85%] mx-auto bg-[#212F39] py-16 px-12 rounded-3xl my-20 flex items-center ">
+			<img src={free} alt="" className="cursor-pointer  hover:scale-105 ease-in duration-500" />
+			<div className="ml-40">
+				<h1 className="text-[40px] mb-5 font-bold">Try it for free</h1>
+				<p className="text-[#dddddd] font-bold lg:w-[80%] text-xl">
+					Create your own meaningful link in bio today for free. Join for free!
+				</p>
+				<button className=" text-xl text-white  py-2 px-10 mt-10  bg-[#0082ff] hover:bg-[#0168c8] focus:ring-[#4ea4f4] focus:ring-offset-[#0082FF]   transition ease-in duration-200 text-center  font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-full">
+					Get Started
+				</button>
+			</div>
+		</div>
+	);
+};
+
+export default TryForFree;
